fc4d05601cadc3cfacf81521ef7aec68f7499b26
Ian Walter <public@iankwalter.com>
2018-11-08 16:10:39 -0500

    Adding linting

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,21 @@ const { join } = require('path')
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
+const isProduction = process.env.NODE_ENV === 'production'
+const src = join(__dirname, 'src')
+
 module.exports = {
-  mode: process.env.NODE_ENV || 'development',
+  mode: isProduction ? 'production' : 'development',
   entry: join(__dirname, 'src/main.js'),
+  output: {
+    filename: 'js/[name].bundle.js',
+    chunkFilename: 'js/[id].[chunkhash].js'
+  },
   resolve: {
-    extensions: ['.js', '.vue', '.json']
+    extensions: ['.js', '.vue', '.json'],
+    alias: {
+      '@': src
+    }
   },
   module: {
     rules: [
