5d455b91b5749b148761da9267f7595d76ecfe21
Ian Walter <public@iankwalter.com>
2018-11-22 22:16:03 -0500

    Adding CleanWebpackPlugin

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    Hello World!
+    Sup World!
   </div>
 </template>
--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,38 @@
 const { join } = require('path')
 
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 const isProduction = process.env.NODE_ENV === 'production'
 const src = join(__dirname, 'src')
 
-module.exports = {
-  mode: isProduction ? 'production' : 'development',
-  entry: join(__dirname, 'src/main.js'),
-  output: {
-    filename: 'js/[name].bundle.js',
-    chunkFilename: 'js/[id].[chunkhash].js'
-  },
-  resolve: {
-    extensions: ['.js', '.vue', '.json'],
-    alias: {
-      '@': src
-    }
-  },
-  module: {
-    rules: [
-      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
-      { test: /\.vue$/, exclude: /node_modules/, loader: 'vue-loader' }
+module.exports = (env = {}) => {
+  return {
+    mode: isProduction ? 'production' : 'development',
+    entry: join(__dirname, 'src/main.js'),
+    output: {
+      filename: 'js/[name].[chunkhash].js',
+      chunkFilename: 'js/[id].[chunkhash].js'
+    },
+    resolve: {
+      extensions: ['.js', '.vue', '.json'],
+      alias: {
+        '@': src
+      }
+    },
+    module: {
+      rules: [
+        { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+        { test: /\.vue$/, exclude: /node_modules/, loader: 'vue-loader' }
+      ]
+    },
+    plugins: [
+      ...(env.singleRun ? [
+        new CleanWebpackPlugin(['dist'])
+      ] : []),
+      new VueLoaderPlugin(),
+      new HtmlWebpackPlugin({ template: './src/index.html' })
     ]
-  },
-  plugins: [
-    new VueLoaderPlugin(),
-    new HtmlWebpackPlugin({ template: './src/index.html' })
-  ]
+  }
 }
