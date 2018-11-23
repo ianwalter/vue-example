3d69619614078c7bb9c537ef6e8e5a457026132e
Ian Walter <public@iankwalter.com>
2018-11-22 22:19:11 -0500

    Fixing webpack config export

--- webpack.config.js
+++ webpack.config.js
@@ -7,32 +7,30 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const isProduction = process.env.NODE_ENV === 'production'
 const src = join(__dirname, 'src')
 
-module.exports = (env = {}) => {
-  return {
-    mode: isProduction ? 'production' : 'development',
-    entry: join(__dirname, 'src/main.js'),
-    output: {
-      filename: 'js/[name].[chunkhash].js',
-      chunkFilename: 'js/[id].[chunkhash].js'
-    },
-    resolve: {
-      extensions: ['.js', '.vue', '.json'],
-      alias: {
-        '@': src
-      }
-    },
-    module: {
-      rules: [
-        { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
-        { test: /\.vue$/, exclude: /node_modules/, loader: 'vue-loader' }
-      ]
-    },
-    plugins: [
-      ...(env.singleRun ? [
-        new CleanWebpackPlugin(['dist'])
-      ] : []),
-      new VueLoaderPlugin(),
-      new HtmlWebpackPlugin({ template: './src/index.html' })
+module.exports = (env = {}) => ({
+  mode: isProduction ? 'production' : 'development',
+  entry: join(__dirname, 'src/main.js'),
+  output: {
+    filename: 'js/[name].[chunkhash].js',
+    chunkFilename: 'js/[id].[chunkhash].js'
+  },
+  resolve: {
+    extensions: ['.js', '.vue', '.json'],
+    alias: {
+      '@': src
+    }
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+      { test: /\.vue$/, exclude: /node_modules/, loader: 'vue-loader' }
     ]
-  }
-}
+  },
+  plugins: [
+    ...(env.singleRun ? [
+      new CleanWebpackPlugin(['dist'])
+    ] : []),
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({ template: './src/index.html' })
+  ]
+})
